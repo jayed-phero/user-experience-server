959172a64f323833c9f91e8e8245243a330e26db
jayed-phero <jayed.programminghero@gmail.com>
2022-11-08 00:48:57 +0600

    second commit for checking

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const port = 
+
+app.use(cors())
+app.use(json())
+
+// echo "# b6a11-service-review-server-side-jayed-phero" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Porgramming-Hero-web-course/b6a11-service-review-server-side-jayed-phero.git
+// git push -u origin main
