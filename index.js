b536314f19f4ea1658b3c0dd55d45fe9d5fab3cb
jayed-phero <jayed.programminghero@gmail.com>
2022-11-08 08:09:51 +0600

    commit of mongodb connect and others

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
 const express = require('express')
 const cors = require('cors')
+const app = express();
+require('dotenv').config()
 
-const port = 
+const port = process.env.PORT || 5000;
 
 app.use(cors())
-app.use(json())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_USER;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+        const serviceCollection = client.db('reviewAssignment').collection('services')
+
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    } 
+    finally{
+        
+    }
+}
+run()
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Review assignment server is running ')
+})
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // echo "# b6a11-service-review-server-side-jayed-phero" >> README.md
 // git init
